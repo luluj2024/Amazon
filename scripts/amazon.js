1578b135a3fa6880e238c77ddf499c069814d359
lulu <lulujiang505@gmail.com>
2025-01-03 11:08:37 -0800

    Generated Order Summat

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 
@@ -56,33 +57,24 @@ products.forEach( (product) => {
 
 document.querySelector('.products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity(){
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+        cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-        let matchingItem;
 
-        // check if already in the cart
-        cart.forEach((item) => {
-            if (item.id === productId) {
-                matchingItem = item;
-            } 
-        });
+        addToCart(productId);
+        updateCartQuantity();
 
-        if (matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            cart.push({
-                id: productId,
-                quantity: 1
-            });
-        }
-
-        let cartQuantity = 0;
-
-        cart.forEach((item) => {
-            cartQuantity += item.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        console.log(cart);
     });
 });
