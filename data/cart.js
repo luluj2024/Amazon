9d4833a0daa963bb3341c4b0fa0258bf567b0048
lulu <lulujiang505@gmail.com>
2025-01-03 12:46:56 -0800

    Updated Delievery Option

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,26 @@
-export let cart = [{
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-},
-{
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {
+
+    // default value
+    cart = [{
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2, 
+        deliveryOptionId: '1'
+    },
+    {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+}
+
+
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -22,9 +37,13 @@ export function addToCart(productId){
     } else {
         cart.push({
             id: productId,
-            quantity: 1
+            quantity: 1,
+            // default deliverty option for new product
+            deliveryOptionId: '1'
         });
     }
+
+    saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -38,5 +57,6 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
 
 }
