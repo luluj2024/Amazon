3ecdfffed003f7947ea6cd76824899afd9455096
lulu <lulujiang505@gmail.com>
2025-01-03 14:35:48 -0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { format } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
 
@@ -112,7 +113,7 @@ export function renderOrderSummary(){
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
-
+      renderPaymentSummary();
     });
 
   });
@@ -123,6 +124,8 @@ export function renderOrderSummary(){
       updateDeliveryOption(productId, deliveryOptionId);
       // update the data -> regenerate all the HTML
       renderOrderSummary();
+      renderPaymentSummary();
+      
     });
   });
 }
